4baa64b4f954137e5ad6ad6f961b8395df7736e3
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-06 15:16:03 +0200

    Add methods to tracker class

--- CycleTrack-Console/Tracker.cs
+++ CycleTrack-Console/Tracker.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CycleTrack_Console {
     internal class Tracker {
+
+        private List<Period> periods = new List<Period>();
+
+        public void AddHistory(Period period) {
+
+        }
+
+        public Period CalculateNext() {
+            return new Period();
+        }
+
+        private void SaveData() {
+
+        }
+
+        private void LoadData() { 
+        
+        }
     }
 }
